34b74ccc5d71ae1d9a2accc4e37392c8140d0bbf
BrayanJakes <brianjakes22@gmail.com>
2019-05-19 09:53:37 -0400

    otros mas

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
 // const errorHandler = require('errorhandler');
@@ -26,13 +27,14 @@ module.exports = app => {
   app.use(morgan('dev'));
   app.use(express.urlencoded({extended: false}));
   app.use(express.json());
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://imgshark.herokuapp.com' );
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+  app.use(cors({origin: 'https://imgshark.herokuapp.com'}))
+//   app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://imgshark.herokuapp.com' );
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
   // Routes
   routes(app);
