d249466e35be86f07e67951f4d51abf6f9ac0537
BrayanJakes <brianjakes22@gmail.com>
2019-05-18 22:59:56 -0400

    otros mas

--- src/server/config.js
+++ src/server/config.js
@@ -26,13 +26,13 @@ module.exports = app => {
   app.use(morgan('dev'));
   app.use(express.urlencoded({extended: false}));
   app.use(express.json());
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://imgshark.herokuapp.com' );
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+//   app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://imgshark.herokuapp.com' );
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
   // Routes
   routes(app);
