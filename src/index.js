9799965d1691f795f480d983cf4e00e2aaa052c2
BrayanJakes <brianjakes22@gmail.com>
2019-05-18 22:57:07 -0400

    otros mas

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ require('./database');
 // Socket.io
 module.exports.io = socketIO(server);
 require('./socket/socket');
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://imgshark.herokuapp.com' );
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // Starting the server
 server.listen(app.get('port'), () => {
