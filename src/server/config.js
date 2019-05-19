85d26e88ff84eac3e381f5903711214553bbc17d
BrayanJakes <brianjakes22@gmail.com>
2019-05-19 09:39:23 -0400

    otros mas

--- src/server/config.js
+++ src/server/config.js
@@ -27,7 +27,7 @@ module.exports = app => {
   app.use(express.urlencoded({extended: false}));
   app.use(express.json());
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://imgshark.herokuapp.com' );
+    res.header('Access-Control-Allow-Origin', 'https://imgshark.herokuapp.com' );
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
